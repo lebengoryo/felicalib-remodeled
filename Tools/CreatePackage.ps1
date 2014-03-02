b1366076cdff304652a610c79272b85ab8a278e3
Keiho Sakapon <github@saka-pon.net>
2014-03-02 12:37:12 +0900

    Add tool to create package

---
+++ Tools/CreatePackage.ps1
@@ -0,0 +1,10 @@
+﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+
+cd ..\FelicaLib_Remastered
+Invoke-Expression ($msbuild + " FelicaLib_Remastered.sln /p:Configuration=Release /t:Clean")
+Invoke-Expression ($msbuild + " FelicaLib_Remastered.sln /p:Configuration=Release /t:Rebuild")
+
+cd .\FelicaLib_DotNet
+.\NuGetPackup.exe
+
+move *.nupkg ..\..\Published -Force
