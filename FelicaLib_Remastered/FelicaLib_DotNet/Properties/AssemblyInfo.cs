889ab52a6b32fb5b1bf8fe0e31687b4ee6710ed7
Keiho Sakapon <github@saka-pon.net>
2014-03-02 12:18:04 +0900

    Update project settings

--- FelicaLib_Remastered/FelicaLib_DotNet/Properties/AssemblyInfo.cs
+++ FelicaLib_Remastered/FelicaLib_DotNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("FelicaLib_DotNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("felicalib Remastered")]
+[assembly: AssemblyDescription("FeliCa access wrapper library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FelicaLib_DotNet")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("felicalib")]
+[assembly: AssemblyCopyright("© 2014 Keiho Sakapon.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
