1e714adeb0c6f2a109edfa78a6fa0660406c17fd
Keiho Sakapon <github@saka-pon.net>
2014-03-02 17:28:00 +0900

    Add unit test projects

---
+++ FelicaLib_Remastered/UnitTest45/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest45
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
