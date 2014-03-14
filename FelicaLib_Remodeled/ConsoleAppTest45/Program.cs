3d63972716f88c0f804164adbcede1fac87d8e2b
Keiho Sakapon <github@saka-pon.net>
2014-03-14 14:16:13 +0900

    Update test method

--- FelicaLib_Remodeled/ConsoleAppTest45/Program.cs
+++ FelicaLib_Remodeled/ConsoleAppTest45/Program.cs
@@ -16,23 +16,40 @@ namespace ConsoleAppTest45
             {
                 for (int i = 0; i < 100; i++)
                 {
-                    try
-                    {
-                        Console.WriteLine("Start");
+                    Console.WriteLine("Start");
 
-                        using (var felica = new Felica(FelicaSystemCode.Edy))
+                    using (var felica = new Felica(FelicaSystemCode.Edy))
+                    {
+                        try
                         {
                             Console.WriteLine(felica.GetIDm().ToHexString());
                         }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        try
+                        {
+                            Console.WriteLine(felica.GetPMm().ToHexString());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        try
+                        {
+                            var data = felica.ReadWithoutEncryption(0x1317, 0);
+                            Console.WriteLine(ToEdyBalance(data));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
                     }
                 }
             });
 
-            Thread.Sleep(10000);
+            Thread.Sleep(15000);
             //Console.WriteLine("Press [Enter] key to exit.");
             //Console.ReadLine();
         }
@@ -41,5 +58,10 @@ namespace ConsoleAppTest45
         {
             return string.Concat(data.Select(b => b.ToString("X2")));
         }
+
+        static readonly Func<byte[], int> ToEdyBalance = b => b
+            .Take(4)
+            .Select((v, i) => v * (int)Math.Pow(256, i))
+            .Sum();
     }
 }
