f8150194e8a84dd98b3f7fcc5d3a57243de2fdcb
Keiho Sakapon <github@saka-pon.net>
2014-03-16 11:36:22 +0900

    Add test methods

---
+++ FelicaLib_Remodeled/UnitTest45/FelicaUtilityTest.cs
@@ -0,0 +1,52 @@
+﻿using FelicaLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest45
+{
+    [TestClass]
+    public class FelicaUtilityTest
+    {
+        [TestMethod]
+        public void GetIDm_1()
+        {
+            var target = FelicaUtility.GetIDm(0xFE00);
+            Assert.AreEqual("0123456789ABCDEF", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void GetIDm_2()
+        {
+            var target = FelicaUtility.GetIDm(FelicaSystemCode.Edy);
+            Assert.AreEqual("0123456789ABCDEF", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void GetPMm_1()
+        {
+            var target = FelicaUtility.GetPMm(0xFE00);
+            Assert.AreEqual("0123456789ABCDEF", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void GetPMm_2()
+        {
+            var target = FelicaUtility.GetPMm(FelicaSystemCode.Edy);
+            Assert.AreEqual("0123456789ABCDEF", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void GetEdyBalance_1()
+        {
+            var target = FelicaUtility.GetEdyBalance();
+            Assert.AreEqual(12345, target);
+        }
+
+        [TestMethod]
+        public void GetSuicaBalance_1()
+        {
+            var target = FelicaUtility.GetSuicaBalance();
+            Assert.AreEqual(12345, target);
+        }
+    }
+}
