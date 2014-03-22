caa77b131d3397dc88f85b63f91cc627b3bfcf30
Keiho Sakapon <github@saka-pon.net>
2014-03-22 20:00:20 +0900

    Add test scenario

---
+++ FelicaLib_Remodeled/UnitTest45/Scenarios/NoPortTest.cs
@@ -0,0 +1,67 @@
+﻿using FelicaLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest45.Scenarios
+{
+    /// <summary>
+    /// FeliCa ポートが接続されていない場合のテストです。
+    /// </summary>
+    [TestClass]
+    public class NoPortTest
+    {
+        [TestMethod]
+        public void Felica_TryConnectionToPort()
+        {
+            using (var felica = new Felica(FelicaSystemCode.Any))
+            {
+                Assert.IsFalse(felica.TryConnectionToPort());
+            }
+        }
+
+        [TestMethod]
+        public void Felica_TryConnectionToCard()
+        {
+            using (var felica = new Felica(FelicaSystemCode.Any))
+            {
+                Assert.IsFalse(felica.TryConnectionToCard());
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Felica_GetIDm()
+        {
+            using (var felica = new Felica(FelicaSystemCode.Any))
+            {
+                felica.GetIDm();
+            }
+        }
+
+        [TestMethod]
+        public void FelicaUtility_TryConnectionToPort()
+        {
+            Assert.IsFalse(FelicaUtility.TryConnectionToPort());
+        }
+
+        [TestMethod]
+        public void FelicaUtility_TryConnectionToCard()
+        {
+            Assert.IsFalse(FelicaUtility.TryConnectionToCard(FelicaSystemCode.Any));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FelicaUtility_GetIDm()
+        {
+            FelicaUtility.GetIDm(FelicaSystemCode.Any);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FelicaHelper_GetEdyBalance()
+        {
+            FelicaHelper.GetEdyBalance();
+        }
+    }
+}
