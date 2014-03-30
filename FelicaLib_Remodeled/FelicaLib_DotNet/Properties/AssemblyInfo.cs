bdde6680dd0195b3d2ebfaf9719464e75e2c3bb2
Keiho Sakapon <github@saka-pon.net>
2014-03-31 00:33:47 +0900

    Add FelicaWatcher class

--- FelicaLib_Remodeled/FelicaLib_DotNet/Properties/AssemblyInfo.cs
+++ FelicaLib_Remodeled/FelicaLib_DotNet/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ A FeliCa access wrapper library for .NET.")]
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.70.0")]
-[assembly: AssemblyFileVersion("1.3.70.0")]
+[assembly: AssemblyVersion("1.3.71.0")]
+[assembly: AssemblyFileVersion("1.3.71.0")]
 
 [assembly: CLSCompliant(true)]
