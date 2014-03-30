eb56899e37e6e9ed83f923200a1babcd4d4d2c43
Keiho Sakapon <github@saka-pon.net>
2014-03-31 00:11:17 +0900

    Update install.ps1 for x64

--- FelicaLib_Remodeled/FelicaLib_DotNet/pkg/tools/install.ps1
+++ FelicaLib_Remodeled/FelicaLib_DotNet/pkg/tools/install.ps1
@@ -3,3 +3,7 @@
 $felicalibItem = $project.ProjectItems.Item("felicalib.dll")
 $outputProperty = $felicalibItem.Properties.Item("CopyToOutputDirectory")
 $outputProperty.Value = 2
+
+$felicalib64Item = $project.ProjectItems.Item("felicalib64.dll")
+$outputProperty64 = $felicalib64Item.Properties.Item("CopyToOutputDirectory")
+$outputProperty64.Value = 2
