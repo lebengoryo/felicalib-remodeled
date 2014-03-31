383ee25389d1933fafa4c79b69e27a5032c127aa
Keiho Sakapon <github@saka-pon.net>
2014-03-31 15:29:30 +0900

    Add FelicaWatcherTest class

---
+++ FelicaLib_Remodeled/UnitTest45/FelicaWatcherTest.cs
@@ -0,0 +1,30 @@
+﻿using FelicaLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UnitTest45
+{
+    [TestClass]
+    public class FelicaWatcherTest
+    {
+        [TestMethod]
+        public void FelicaWatcher_1()
+        {
+            using (var watcher = new FelicaWatcher(FelicaSystemCode.Edy))
+            {
+                watcher.CardArrived += f =>
+                {
+                    Debug.WriteLine(FelicaHelper.GetEdyBalance());
+                };
+                watcher.CardDeparted += f =>
+                {
+                    Debug.WriteLine("Card Departed");
+                };
+
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
